881bc0806508858c6bb7a955cb0923cdf2598231
Linh <nguyentanlinh90@gmail.com>
2022-05-20 12:25:45 +0700

    add model bottom sheet

--- app/src/main/java/com/ntl/guidelinesapp/modules/bottom_sheet/BottomSheetActivity.java
+++ app/src/main/java/com/ntl/guidelinesapp/modules/bottom_sheet/BottomSheetActivity.java
@@ -2,19 +2,19 @@ package com.ntl.guidelinesapp.modules.bottom_sheet;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.ntl.guidelinesapp.R;
 
 public class BottomSheetActivity extends AppCompatActivity {
-    private RelativeLayout rlBottomSheet;
-    private Button btPersistent, btModel, btCloseBottomSheet;
-    private BottomSheetBehavior bottomSheetBehavior;
+    private Button btPersistent, btModel;
+    private BottomSheetDialog modelSheetDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,33 +22,37 @@ public class BottomSheetActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bottom_sheet);
         getSupportActionBar().setTitle("BottomSheetActivity");
 
-        rlBottomSheet = findViewById(R.id.rl_bottom_sheet);
         btPersistent = findViewById(R.id.bt_persistent);
-        btCloseBottomSheet = findViewById(R.id.bt_close_bottom_sheet);
         btModel = findViewById(R.id.bt_model);
-        bottomSheetBehavior = BottomSheetBehavior.from(rlBottomSheet);
 
         btPersistent.setOnClickListener(v -> {
-            if (bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_COLLAPSED) {
-                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
-            } else {
-                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
-            }
+            openPersistentBottomSheetFragment();
         });
 
-        btCloseBottomSheet.setOnClickListener(v -> bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED));
+        btModel.setOnClickListener(v -> {
+            openModelBottomSheetFragment();
+        });
+    }
 
+    public void openModelSheet() {
+
+        View view = getLayoutInflater().inflate(R.layout.model_bottom_sheet, null);
+
+        modelSheetDialog = new BottomSheetDialog(this);
+        modelSheetDialog.setContentView(view);
+
+        // set disable click outside sheetDialog or click backPress
+        modelSheetDialog.setCancelable(false);
+
+        modelSheetDialog.show();
+
+        //set open full screen when open
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from((View) view.getParent());
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
         bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
             public void onStateChanged(@NonNull View bottomSheet, int newState) {
-                switch (newState) {
-                    case BottomSheetBehavior.STATE_EXPANDED:
-                        btPersistent.setText("Persistent CLOSE");
-                        break;
-                    case BottomSheetBehavior.STATE_COLLAPSED:
-                        btPersistent.setText("Persistent OPEN");
-                        break;
-                }
+                //todo
             }
 
             @Override
@@ -56,5 +60,24 @@ public class BottomSheetActivity extends AppCompatActivity {
 
             }
         });
+        closeModelSheet(view);
+    }
+
+    private void closeModelSheet(View view) {
+        view.findViewById(R.id.bt_close_bottom_sheet).setOnClickListener(v -> {
+            modelSheetDialog.dismiss();
+        });
+    }
+
+    private void openPersistentBottomSheetFragment() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fl_content, new PersistentBottomSheetFragment());
+        transaction.commit();
+    }
+
+    private void openModelBottomSheetFragment() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fl_content, new ModelBottomSheetFragment());
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/ntl/guidelinesapp/modules/bottom_sheet/ModelBottomSheetFragment.java
@@ -0,0 +1,75 @@
+package com.ntl.guidelinesapp.modules.bottom_sheet;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.ntl.guidelinesapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ModelBottomSheetFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ModelBottomSheetFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private BottomSheetActivity mActivity;
+
+    public ModelBottomSheetFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ModelBottomSheetFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ModelBottomSheetFragment newInstance(String param1, String param2) {
+        ModelBottomSheetFragment fragment = new ModelBottomSheetFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mActivity = (BottomSheetActivity) getActivity();
+        View view = inflater.inflate(R.layout.fragment_model_bottom_sheet, container, false);
+        view.findViewById(R.id.bt_open).setOnClickListener(v -> {
+            mActivity.openModelSheet();
+        });
+
+        return view;
+    }
+}
